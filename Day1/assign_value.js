9e7487c76a5b39347da15da0a6a1eae84290b270
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2025-12-31 14:10:51 +0530

    Day 1

---
+++ Day1/assign_value.js
@@ -0,0 +1,71 @@
+
+
+// // This all Are simeple Define...
+
+// // let a = 4;
+// // var b = 5;
+// // const c = 10;
+
+// // console.log(a);
+// // console.log(b);
+// // console.log(c);
+
+
+
+// // Some Different Example to show you..
+
+// let d = 45;
+// d = 5;
+// console.log(d);  // 5 Output . Beacuase value is chagened at second line .. But in COnst() It's fix
+
+// // Const USe
+
+// const m = 3.14;
+// // a = 4;     show the error becuase value is fixed...
+// console.log(m);  // 3.14
+
+
+
+
+// Now USe to Print Full name 
+
+// let FirstName= "Harsh";
+// let lastName = "Singh";
+// let fullName = FirstName + " " + lastName;
+
+
+// console.log(FirstName);   // Harsh
+// console.log(lastName);     //Singh
+// console.log(fullName);     //Harsh Singh
+
+
+
+
+
+ // Checking typeof 
+// let a = 2.4;
+
+// a = true;
+
+// a = "Tej";
+
+// a = 449348.398398439
+
+
+//  console.log(typeof a); // number;
+
+
+
+
+// USeing TRicky THings
+
+// let a = 4;
+
+// console.log("4" -  8);  // -4;
+// console.log("4" +  8);  // 48;
+// console.log("4" *  8);  // 32;
+
+// console.log("Ram" - 3)  // NaN;
+
+
+
