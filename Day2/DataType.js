917535d55cd7a4cde83ce6e52903a0537f265178
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2025-12-31 14:14:56 +0530

    Day2

---
+++ Day2/DataType.js
@@ -0,0 +1,7 @@
+let name = "Tejasvi";   // String
+let age = 21;          // Number
+let isStudent = true;  // Boolean
+let x = null;          // Null
+let y;                 // Undefined
+let id = Symbol("id"); // Symbol
+let big = 123n;        // BigInt
