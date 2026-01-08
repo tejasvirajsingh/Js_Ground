ad2c949b9da1eda2c31babc2534ca1e15ef2009c
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-01-09 00:27:14 +0530

    converstion

---
+++ Day4/converstionOperation.js
@@ -0,0 +1,19 @@
+// when we conver inot number to Boolean..
+
+// let a= 1;
+
+// let booleana = Boolean(a);
+// console.log(booleana)
+
+
+
+
+
+
+// now convert into number to String.
+
+let num = 45;
+let string = String(num)
+console.log(string);           // 45
+console.log(typeof num);      // number
+console.log(typeof string);   // String
