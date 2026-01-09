e075f9e51e30a878c5ab3ea992a31c60e9fc4332
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-01-09 23:23:28 +0530

    max

---
+++ Day4/operators.js
@@ -0,0 +1,17 @@
+// let a = 5;
+
+// a = a + 5;
+
+// console.log(a);   // 10
+  
+//   Using of assingment operators..
+
+// let a = 5; 
+// //  a += 6;
+// //  a %= 5;
+// //  a -= 2;
+// //  a *= 4;
+
+// a /= 5;
+
+//  console.log(a);
