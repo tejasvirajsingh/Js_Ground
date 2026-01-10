d4b88dee98aaa1b2bb8f8fc033ce70381b894bf4
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-01-10 20:28:24 +0530

    some example add

--- Day4/tempCodeRunnerFile.js
+++ Day4/tempCodeRunnerFile.js
@@ -1,13 +1,40 @@
-//  a += 6;
-//  a %= 5;
-//  a -= 2;
-//  a *= 4;
+// // Assing Operator 
 
 
-// Increatment or Decerement operator.
+// //  a += 6;
+// //  a %= 5;
+// //  a -= 2;
+// //  a *= 4;
 
-let x = 3;
 
-x++;  // 4
-x--;  // 3
-console.log(x);  // 3
+// // Increatment or Decerement operator.
+
+// let x = 3;
+
+// // x++;  // 4
+// // x--;  // 3
+// console.log(x);  // 3
+
+// console.log(x++); // 3
+
+// console.log(x); // 4
+
+// console.log(--x); // 3
+
+// Let's take example..
+
+// let a = 10;
+
+// let result = a++ + ++a - 10 ;
+
+// console.log(result);  // 22 - 10 = 12.
+
+
+
+// Another example
+
+let i = 0;
+
+while (i++ < 3) {
+  console.log(i);
+}
