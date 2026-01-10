c3563cafa78ffac5130dfbe6d2b74ab44c816c57
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-01-10 22:33:37 +0530

    Ternary Operator

---
+++ Day4/ternaryOper.js
@@ -0,0 +1,19 @@
+let age = 4;
+
+let Reason = age > 18 ? "Eligible for vote" : "Not allowed for Vote , Because under 18";
+
+console.log("Reason: " + Reason);  //Reason: Not allowed for Vote , Because under 18
+
+
+/* 
+✅ Ternary Operator Syntax (Remember This)
+
+  -> condition ? value_if_true : value_if_false
+
+
+Example:
+
+age > 18 ? "Eligible" : "Not Eligible"
+
+
+*/
