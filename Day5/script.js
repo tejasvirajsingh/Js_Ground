f3bf24b08f7f132ad25f890a40223b2cab89ba02
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-01-11 23:49:52 +0530

    == and === difference

--- Day5/script.js
+++ Day5/script.js
@@ -1,4 +1,39 @@
 
 
-let a = number(prompt("Enter a first Num: "));
-let b = number(prompt(" Enter a second Num"))
+// let a = Number(prompt("Enter a first Num: "));
+// let b = Number(prompt(" Enter a second Num: "));
+
+// console.log("a + b = ", a + b );
+// console.log("a - b = ", a - b );
+// console.log("a * b = ", a * b );
+// console.log("a / b = ", a / b );
+// console.log("a ** b = ", a ** b );
+
+
+
+
+
+
+
+// Let's check it Number is odd or even 
+
+let a = Number(prompt("Enter a number: "));
+
+let result = ( a % 2 === 0) ? "even" : "odd" ;
+
+console.log(result);
+
+
+
+
+
+
+// == and === difference 
+
+// case 1 (==) is use to check the value is equal or not ...
+// case 2 (===) is use to check the data type and value also then . 
+
+
+// case 1 .  4 == "4"  // true
+// case 2.   4 === "4"  // false
+
