27c8d0fe27a31257a25c1fbdd710f9e2671597ac
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-01-12 15:48:29 +0530

    update

--- Day6/tricky.js
+++ Day6/tricky.js
@@ -1,9 +1,22 @@
 
 
-// console.log(undefined + 1);
+// // console.log(undefined + 1);
 
-let fruit = "Apple , Mango , Orange, Banana , Stwaberry, Poemgrante";
+// let fruit = "Apple , Mango , Orange, Banana , Stwaberry, Poemgrante";
 
-let arr = fruit.split(", ");
+// let arr = fruit.split(", ");
 
+// console.log(arr);
+
+
+
+// New one
+
+// console.log([2,3] + [5,6]);
+
+
+// New One
+
+let arr= [ 2,3,4,5,4,5,6,7,8,83,23,43,5,5,11];
+arr.length = 6;
 console.log(arr);
