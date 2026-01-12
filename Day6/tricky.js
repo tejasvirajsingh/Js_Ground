f554d5c5cbe77db900bb0fca3a2f66478b545f94
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-01-12 15:58:58 +0530

    update

--- Day6/tricky.js
+++ Day6/tricky.js
@@ -17,6 +17,13 @@
 
 // New One
 
-let arr= [ 2,3,4,5,4,5,6,7,8,83,23,43,5,5,11];
-arr.length = 6;
-console.log(arr);
+// let arr= [ 2,3,4,5,4,5,6,7,8,83,23,43,5,5,11];
+// arr.length = 6;
+// console.log(arr);
+
+
+
+
+// Checking array adding between clg..
+
+// console.log([2,3] + [5,6]);
