8168ef803b3e3e85acf2fe111502bf3d30b5c54a
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-01-13 07:59:19 +0530

    If_else

---
+++ Day6/if_else_else_if.js
@@ -0,0 +1,20 @@
+let score = 89;
+
+
+if(score>= 90 && score<=100){
+console.log("Grade A: " + score)
+
+} else if(score>=80 && score<=89){
+    console.log("Grade B: " + score)
+
+}else if(score>=70 && score<= 79){
+    console.log("Grade C: " + score)
+
+} else if(score>= 60 && score<= 69){
+    console.log("Passed: " + score)
+} 
+else if(score => 0  && score <= 59)
+{
+    console.log("Fail . You Need to Study Hard: " + score)
+}
+
