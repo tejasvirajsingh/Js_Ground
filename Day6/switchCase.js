fa844beca39cdf42ec1e9e9fedeaf5182a2c715d
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-01-14 22:42:20 +0530

    switch case

---
+++ Day6/switchCase.js
@@ -0,0 +1,38 @@
+// let letter = "h";
+
+// switch (letter) {
+//   case "a":
+//   case "e":
+//   case "i":
+//   case "o":
+//   case "u":
+//     console.log("Vowel");
+//     break;
+
+//   default:
+//     console.log("Consonant");
+// }
+
+
+
+
+// New Examples: 
+
+let color = "Red";
+
+switch (color) {
+
+case "Green" : console.log("Now Go!");
+              break;
+
+              case "Yellow" : console.log("Ready For Go...");
+              break;
+
+              case "Orange" : console.log("Now Start To Go");
+              break;
+
+              default:
+              console.log("Stop");
+              break;
+
+}
