329fdeab22c97f0325cfc1b70d469fc26e924763
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-01-14 22:22:32 +0530

    check the password is correct or not

---
+++ Day6/checkingPass.js
@@ -0,0 +1,13 @@
+
+let username = "HarshRajput";
+ let password = 99000;
+
+let dbusername = "HarshRajput";
+let dbpassword = 99000;
+
+if( username === dbusername && password === dbpassword ) {
+    console.log("Succesfully login")
+
+} else {
+    console.log("Invalid password")
+}
