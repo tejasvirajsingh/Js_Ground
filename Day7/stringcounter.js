8c9d1c64e77461ab05cdc35261ff05c322d5b245
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-01-16 01:17:09 +0530

    string length counter

---
+++ Day7/stringcounter.js
@@ -0,0 +1,17 @@
+let a = "Harsh singh is Be Coder.";
+let b = "Tejasvi Is be Hacker of World.";
+
+console.log(a.length); // => 24;
+console.log(b.length);  // => 30;
+
+
+
+
+
+
+// let single = 'Wheres my bandit hat?';   //=> Single string code
+// let double = "Wheres my bandit hat?";    // => Double string code
+
+
+// console.log(single.length);  // => 21
+
