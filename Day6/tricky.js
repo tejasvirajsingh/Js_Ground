b66ba6446fbad4e716e4e7b79609cabc86948635
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-01-17 15:05:03 +0530

    Use SetTimeout() fun

--- Day6/tricky.js
+++ Day6/tricky.js
@@ -1,69 +1,69 @@
 
 
-// // console.log(undefined + 1);
+// // // console.log(undefined + 1);
 
-// let fruit = "Apple , Mango , Orange, Banana , Stwaberry, Poemgrante";
+// // let fruit = "Apple , Mango , Orange, Banana , Stwaberry, Poemgrante";
 
-// let arr = fruit.split(", ");
+// // let arr = fruit.split(", ");
 
-// console.log(arr);
-
-
-
-// New one
-
-// console.log([2,3] + [5,6]);
-
-
-// New One
-
-// let arr= [ 2,3,4,5,4,5,6,7,8,83,23,43,5,5,11];
-// arr.length = 6;
 // // console.log(arr);
 
 
 
-
-// // Checking array adding between clg..
+// // New one
 
 // // console.log([2,3] + [5,6]);
 
 
+// // New One
+
+// // let arr= [ 2,3,4,5,4,5,6,7,8,83,23,43,5,5,11];
+// // arr.length = 6;
+// // // console.log(arr);
+
+
+
+
+// // // Checking array adding between clg..
+
+// // // console.log([2,3] + [5,6]);
 
 
 
 
 
 
-// // if i have to sort the array ..
-
-// // let arr = [ 4,5,7,3,6,2].sort();
-
-// // console.log(arr);
 
 
-// // when using spread operator 
+// // // if i have to sort the array ..
 
-let a = [3,45,3,6,2,66,7,9];
-let b = [5,44,65,"Ram", 8];
+// // // let arr = [ 4,5,7,3,6,2].sort();
 
-// let result = [...a,...b];
-// console.log(result);
-
-// // suppose i have to find length of array then use.
-// console.log(result.length); // >13.
-// // suppose i have to display only index [5] thne
-// console.log(result[5]);  // > 66
+// // // console.log(arr);
 
 
-// // when i have to convert into string.
-// console.log(result.join(" ,"));
-// // > 3, 45, 3, 6, 2, 66, 7, 9, 5, 44, 65, Ram, 8
+// // // when using spread operator 
+
+// let a = [3,45,3,6,2,66,7,9];
+// let b = [5,44,65,"Ram", 8];
+
+// // let result = [...a,...b];
+// // console.log(result);
+
+// // // suppose i have to find length of array then use.
+// // console.log(result.length); // >13.
+// // // suppose i have to display only index [5] thne
+// // console.log(result[5]);  // > 66
 
 
-// // or Making in JSON format
-// console.log(JSON.stringify(result));
-// // > [3,45,3,6,2,66,7,9,5,44,65,"Ram",8]
+// // // when i have to convert into string.
+// // console.log(result.join(" ,"));
+// // // > 3, 45, 3, 6, 2, 66, 7, 9, 5, 44, 65, Ram, 8
+
+
+// // // or Making in JSON format
+// // console.log(JSON.stringify(result));
+// // // > [3,45,3,6,2,66,7,9,5,44,65,"Ram",8]
 
  
 
@@ -76,10 +76,22 @@ let b = [5,44,65,"Ram", 8];
 
 
 
-// Using Double and single String Quotes. "" , ' '
+// // Using Double and single String Quotes. "" , ' '
 
-let mr = 'How are you ?';
-let max = "I'm fine. And what about you.";
+// let mr = 'How are you ?';
+// let max = "I'm fine. And what about you.";
  
 
-console.log(` ${mr}, "Max:" ${max}`);  //  How are you ?, "Max:" I'm fine. And what about you.
+// console.log(` ${mr}, "Max:" ${max}`);  //  How are you ?, "Max:" I'm fine. And what about you.
+
+
+
+console.log("How are You ?");
+
+setTimeout(() => {
+  console.log("I'm fine mr max."); // >  after 3 sec give > I'm fine mr max.
+  console.log("good")  ;
+}, 3000);
+
+
+
