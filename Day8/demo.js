9d500ac68d66384f6bc33f7baa15464bfad9bebe
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-01-21 17:11:28 +0530

    udpate's

---
+++ Day8/demo.js
@@ -0,0 +1,5 @@
+// var x;
+// console.log(x);
+//     x = 4;
+
+
--- ServerConnect/readfile.js
+++ ServerConnect/readfile.js
@@ -1,20 +1,22 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-// fs.readFile("hello.txt" , "utf-8" , (err, data) =>{
+// // fs.readFile("hello.txt" , "utf-8" , (err, data) =>{
+// //     if(err){
+// //         console.log("Error : " , err);
+
+// //     }else{
+// //         console.log(data);
+// //     }
+// // });
+
+
+
+// fs.mkdir("Coders", (err)=>{
 //     if(err){
-//         console.log("Error : " , err);
-
+//         console.log("Error: ",err);
 //     }else{
-//         console.log(data);
+//         console.log("Folder created..");
 //     }
-// });
+// })
 
-
-
-fs.mkdir("Coders", (err)=>{
-    if(err){
-        console.log("Error: ",err);
-    }else{
-        console.log("Folder created..");
-    }
-})
+console.log([] == ![]);  // True.
