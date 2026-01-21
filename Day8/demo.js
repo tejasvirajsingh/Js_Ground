7d57120f620a6b1873dbe776e517eac0c6763e9d
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-01-21 17:35:30 +0530

    fix

--- Day8/demo.js
+++ Day8/demo.js
@@ -3,3 +3,14 @@
 //     x = 4;
 
 
+// const stu1 = {name : "Harsh" , age:22};
+// const stu2 = {name : "Harsh" , age:22};
+
+// console.log(stu1 === stu2);    // False 
+
+
+
+let  stu1 = {name : "Harsh" , age:22};
+let  stu2 = {name : "Harsh" , age:22};
+
+console.log(stu1 === stu2);    // False 
---
+++ Day8/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+const stu1 = {name : "Harsh" , age:22};
+// const stu2 = {name : "Harsh" , age:22};
+
+// console.log(stu1 === stu2);    // False 
+
