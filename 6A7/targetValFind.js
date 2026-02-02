ccf3e6ef3ca27d731dc3517bf6be15884a67c30d
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-02-03 02:03:33 +0530

    What i thought in class

---
+++ 6A7/targetValFind.js
@@ -0,0 +1,12 @@
+let arr = [4,5,6,7,8,2,1,3];
+let target = 2;
+
+for(let i =0 ; i<arr.length; i++)
+{
+    if(arr[i] === target){
+        console.log("Found at index" , i);
+    } 
+}
+
+
+// console.log("Not Founding Elements.");
